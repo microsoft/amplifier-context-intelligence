e7c423135e2eae63277ee449cae4c1c76b8b92ce
Diego Colombo <>
2026-03-27 16:50:43 +0000

    feat: export ArtifactLifter, RecipeLifter, SkillLifter from field_lifters package
    Updates __all__ from 8 to 11 names to include the three new lifters.

--- context_intelligence_server/handlers/field_lifters/__init__.py
+++ context_intelligence_server/handlers/field_lifters/__init__.py
@@ -1,20 +1,26 @@
 """FieldLifter implementations for DefaultHandler event field extraction."""
 
+from .artifact import ArtifactLifter
 from .base import RESERVED_PROPS, FieldLifter
 from .delegate import DelegateLifter
 from .llm import LlmLifter
 from .navigation import UniversalLifter
 from .prompt import PromptLifter
+from .recipe import RecipeLifter
 from .session import SessionLifter
+from .skill import SkillLifter
 from .tool import ToolLifter
 
 __all__ = [
+    "ArtifactLifter",
     "DelegateLifter",
     "FieldLifter",
     "LlmLifter",
     "PromptLifter",
     "RESERVED_PROPS",
+    "RecipeLifter",
     "SessionLifter",
+    "SkillLifter",
     "ToolLifter",
     "UniversalLifter",
 ]
